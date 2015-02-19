3bae87eb62a6ceb13328ecf34eefa728c2592a53
Goodwin Procter <goodwinprocter@Goodwins-MacBook-Pro.local>
2015-02-19 13:42:50 -0800

    Add project files

---
+++ LoadingConstraintDemo/LoadingOverlaySideBySide.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using MonoTouch.UIKit;
+using System.Drawing;
+using System.Collections.Generic;
+
+namespace LoadingConstraintDemo {
+
+	public class LoadingOverlaySideBySide : UIView {
+		// control declarations
+
+		UIActivityIndicatorView ActivitySpinner;
+
+		UILabel LoadingLabel;
+
+
+		int SpinnerWidth = 20;
+		int SpinnerHeight = 20;
+
+		int LabelMinWidth = 80;
+		int LabelHeight = 22;
+
+		// Will control the margin between the label and spinner
+		int margin = 5;
+
+		public LoadingOverlaySideBySide () : base (UIScreen.MainScreen.Bounds)
+		{
+			// Set the background transparency
+			BackgroundColor = UIColor.FromRGBA (0f, 0f, 0f, 0.65f);
+			AutoresizingMask = UIViewAutoresizing.FlexibleDimensions;
+
+			// create the activity spinner
+			ActivitySpinner = new UIActivityIndicatorView (UIActivityIndicatorViewStyle.White) {
+
+				// Important when using AutoLayout
+				TranslatesAutoresizingMaskIntoConstraints = false,
+			};
+
+			// create and configure the "Loading Data" label
+			LoadingLabel = new UILabel (){ 
+
+				BackgroundColor = UIColor.Clear,
+				TextColor = UIColor.White,
+				Text = "Loading Data...",
+				TextAlignment = UITextAlignment.Center,
+
+				// Important when using AutoLayout
+				TranslatesAutoresizingMaskIntoConstraints = false
+			};
+
+			ActivitySpinner.StartAnimating ();
+
+			AddSubviews (new UIView[]{ ActivitySpinner, LoadingLabel });
+
+			// Important part of setting up constraints. 
+			// Will call UpdateConstraints on the base implementation.
+			//
+			SetNeedsUpdateConstraints ();
+		}
+
+		public override void UpdateConstraints ()
+		{
+			// Check to see if the View needs to update it's constraints
+			if(NeedsUpdateConstraints()) {
+				setupConstraint (); // Uncomment to view Stacked
+			}
+			base.UpdateConstraints (); // Important, always call this last
+		}
+
+
+		/// <summary>
+		/// Fades out the control and then removes it from the super view
+		/// </summary>
+		public void Hide ()
+		{
+			UIView.Animate (
+				0.5, // duration
+				() => {
+					Alpha = 0;
+				},
+				() => {
+					RemoveFromSuperview ();
+				}
+			);
+		}
+
+		#region PrivateMethods
+
+		void setupConstraint() {
+
+			List<NSLayoutConstraint> constraints = new List<NSLayoutConstraint> ();
+
+			// Views and Metrics (tokens) we'll be passing to the Visual Format
+			var viewMetrics = new Object[] { 
+				"label", LoadingLabel,
+				"spinner", ActivitySpinner,
+				"labelWidth", LabelMinWidth,
+				"labelHeight", LabelHeight,
+				"spinnerHeight", SpinnerHeight,
+				"spinnerWidth", SpinnerWidth,
+				"margin", margin
+			};
+
+
+			// First specify the Horizontal Rule 
+			// [Label] -- margin -- // [Spinner]
+			//
+			constraints.AddRange(
+				NSLayoutConstraint.FromVisualFormat(
+					"[label]-margin-[spinner]", 
+					NSLayoutFormatOptions.AlignAllCenterY, 
+					viewMetrics
+				)
+			);
+
+			// Align the LoadingLabel with the View's CenterX position. 
+			// Because of the constraint rule above, the spinner will be centered as well.
+			//
+			constraints.Add (
+				NSLayoutConstraint.Create (
+					LoadingLabel, // View we want to constraint
+					NSLayoutAttribute.CenterX, // Vertically Center
+					NSLayoutRelation.Equal, // Relationship
+					this, // View we want to constrain to
+					NSLayoutAttribute.CenterX, // Attribute to constrain to
+					1, // Multiplier
+
+					// pull the label UP by the spinners height
+					-SpinnerHeight 
+				)
+			);
+
+			// Align the LoadingLabel with the View's CenterY position.
+			// We continue to maintain a constraint on the spinner, so it will be centered as well!
+			//
+			constraints.Add (
+				NSLayoutConstraint.Create(
+					LoadingLabel, // View we want to constrain
+					NSLayoutAttribute.CenterY, // Horizontally center
+					NSLayoutRelation.Equal, // Relationship
+					this, // View we want to constrain to
+					NSLayoutAttribute.CenterY, // Horizontally center
+					1, // Multiplier
+					0 // constant
+				)
+			);
+
+			// Add the constraints to the view
+			AddConstraints (constraints.ToArray ());
+
+		}
+
+
+		#endregion
+	}
+}
---
+++ LoadingConstraintDemo/MainViewController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using MonoTouch.UIKit;
+
+namespace LoadingConstraintDemo
+{
+	public class MainViewController : UITabBarController
+	{
+
+		public override void LoadView ()
+		{
+			base.LoadView ();
+
+			// Add the Stacked and Side by Side loading examples.
+			ViewControllers = new UIViewController[]{ 
+				new UIViewController(){
+					Title = "Stacked",
+					View = new LoadingOverlayStacked ()
+				},
+				new UIViewController() {
+					Title = "Side by Side",
+						View = new LoadingOverlaySideBySide ()
+				}
+			};
+
+		}
+	}
+}
+
